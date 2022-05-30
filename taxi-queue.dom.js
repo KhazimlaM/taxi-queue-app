36579e9c7d70c37642c888cfa4e051d4921fc829
Khazimla <20113497@rcconnect.co.za>
2022-05-30 15:09:10 +0200

    first changes

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -1,11 +1,39 @@
 // write your DOM code here.
-
-
 // DOM element references
+const count = document.querySelector(".count");
+const joinElement = document.querySelector(".join_queue");
+const leaveElement = document.querySelector(".leave_queue");
+
+const joinTaxiElement = document.querySelector(".taxi_queue");
+const counterElement = document.querySelector(".taxi");
+
+const departElement = document.querySelector(".departing")
 
 // create Factory Function instance
-
 const taxiQueue = TaxiQueue();
 
 // DOM events
+var myCounter = 0;
+
+joinElement.addEventListener('click',function (){
+ 
+   myCounter += 1;
+   count.innerHTML = myCounter;
+    
+});
+
+leaveElement.addEventListener('click',function (){
+ 
+    myCounter -= 1;
+    count.innerHTML = myCounter;
+     
+ });
+
+ 
+
+
+
+
+ 
+
 
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,29 +1,43 @@
 function TaxiQueue() {
-
+     var passenger = 0;
+	 var passengerLeaves = 0;
+	 var taxi = 0;
 
 	function joinQueue() {
-
+		if(passenger){
+			passenger ++;
+		}
+   
 	}
 
 	function leaveQueue() {
-
+       if(passengerLeaves){
+		   passenger.pop()
+	   }
 	}
 
 	function joinTaxiQueue() {
-
+      if(taxi){
+      taxi ++;
+	  }
 	}
 
 	function queueLength() {
-
+     return joinQueue.length;
 	}
 
 	function taxiQueueLength() {
-
+     return joinTaxiQueue.length;
 	}
 
 	function taxiDepart(){
-
+    if(taxi){
+     taxi --;
+	} 
+    else if (taxi){
+		joinQueue.pop(12);
 	}
+}
 
 	return {
 		joinQueue,
